ebc4e14d1ad3bdc4f7f3ee472a778aec5ea61bb1
Abhishek <abhi.9811206369@gmail.com>
2021-02-06 12:20:56 +0530

    fix error handling in jwtMiddleware

--- src/middleware/jwtMiddleware.ts
+++ src/middleware/jwtMiddleware.ts
@@ -12,7 +12,8 @@ export const jwtMiddleware = (req: Request, _: Response, next: NextFunction) => 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET) as UserAuthTokenPayload;
     (req as any).user = decoded;
-  } finally {
+  } catch (error) {
+    // console.error(error);
   }
   next();
 };
